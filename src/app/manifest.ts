4409394209a012d575eea69bf342654fc6ce3e69
maytav <maytavbm@gmail.com>
2026-03-16 23:46:02 +0200

    feat: add manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,32 @@
+import {MetadataRoute} from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'מיטב ביטוח ופיננסים',
+        short_name: 'מיטב ביטוח',
+        description: 'השוואת ביטוח נסיעות לחו״ל מכל חברות הביטוח במקום אחד',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#1E3644',
+        theme_color: '#1E3644',
+        lang: 'he',
+        dir: 'rtl',
+        icons: [
+            {
+                src: '/icon-192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/icon-512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+            {
+                src: '/apple-touch-icon.png',
+                sizes: '180x180',
+                type: 'image/png',
+            },
+        ],
+    };
+}
