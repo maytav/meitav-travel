b7d4670dceaa8b26a48b2df065fe0a8f729c2d80
maytav <maytavbm@gmail.com>
2026-03-17 17:55:36 +0200

    test

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import {getApps, initializeApp} from 'firebase/app';
 import {getAnalytics, isSupported} from 'firebase/analytics';
 
+console.log('Checking Project ID:', process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
